f2053141659491c7558bf2c1e3b6e73c75b0141a
Alex Katopodis <akbd256@gmail.com>
2025-12-21 08:21:54 -0700

    fix: Include header

--- src/utils/generate_matrix.cu
+++ src/utils/generate_matrix.cu
@@ -1,5 +1,7 @@
 #include <cstdlib>
 
+#include "generate_matrix.cuh"
+
 void generate_matrix(float* matrix, int size) {
 	for (int i = 0; i < size; i++) {
 		matrix[i] = static_cast<float>(rand()) / RAND_MAX;
